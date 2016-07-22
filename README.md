70736dcd674b4f4a8301c1416a554ba662e411db
lyx <whu_lyx@163.com>
2016-07-22 22:46:25 +0800

    lyxxcy07-22-1

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Yyjq
 An Android based task system for whu students.
+2016-07-22
+完成了开发环境的搭配;
+底部五个Tab切换功能的实现;
+五个图标的绘制;
---
+++ Yyjq/src/edu/whu/cs/lyxxcy/yyjq/MyFragment.java
@@ -0,0 +1,20 @@
+package edu.whu.cs.lyxxcy.yyjq;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class MyFragment extends Fragment{
+	public int resource;
+	public MyFragment(int resource) {
+		this.resource = resource;
+	}
+	@Override 
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {		
+	    View view = inflater.inflate(resource,container,false);
+	    return view;
+	}
+}
